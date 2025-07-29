150ec45d254adb7631a90a007702ac4c2e1915ef
BartMeer <122463563+BartMeer@users.noreply.github.com>
2025-07-30 00:59:08 +0200

    Make PWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true, // Enable PWA in development mode
+      },
+      manifest: {
+        name: "My workout",
+        short_name: "MyW",
+        description: "A Vite-based Progressive Web App",
+        theme_color: "#111827",
+        background_color: "#111827",
+        display: "standalone",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),],
   server: {
     port: 3000,
     open: true,
